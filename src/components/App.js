5aaca48a9bf308f84bd04a2ee23bafafd2346a30
Jez Blackmore <jezblackmore@Jezs-MacBook-Air.local>
2022-01-23 21:48:40 +0000

    split code using suspense

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,36 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 import { ConstProvider } from "../contexts/Context"; 
 
 import "../css/imports.css";
  
+import spinner from '../img/loader/Spinner-1s-200px.svg';
 
-import Header from "../components/header/Header";
-import SideNavMobile from '../components/header/SideNavMobile';
-import AllReviews from "../components/pages/allReviews/AllReviews";
-import Forums from "../components/pages/forums/mainForumPage/Forums1";
-import ForumTopics from "./pages/forums/topicsListPage/ForumTopics";
-import ForumTopic from "./pages/forums//mainTopicPage/ForumTopic";
-import Login from "../components/pages/login/Login";
-import Register from "../components/pages/register/Register";
-import Profile from "../components/pages/profile/Profile";
-/* import ProfileSettings from "../components/pages/userInfo/profileSettings/ProfileSettings"; */
-import MembersDirectory from "../components/pages/membersDirectory/MembersDirectory";
-import SearchPopUp from './search/SearchPopUp';
-import CookiePolicy from "../components/pages/legal/CookiePolicy";
-import PrivacyPolicy from "../components/pages/legal/PrivacyPolicy";
-import TermsAndConditions from "../components/pages/legal/TermsAndConditions";
-import UnauthorisedPermissions from "../components/pages/unauthorised/UnauthorisedPermissions";
-import CookieNotification from "../components/pages/legal/CookieNotification";
-import Footer from "../components/footer/Footer";
-import Error404 from "../components/pages/errors/Error404";
+const Header = React.lazy(() => import('../components/header/Header'));
+const SideNavMobile = React.lazy(() => import('../components/header/SideNavMobile'));
+const AllReviews = React.lazy(() => import('../components/pages/allReviews/AllReviews'));
+const Forums = React.lazy(() => import('../components/pages/forums/mainForumPage/Forums1'));
+const ForumTopics = React.lazy(() => import('./pages/forums/topicsListPage/ForumTopics'));
+const ForumTopic = React.lazy(() => import('./pages/forums//mainTopicPage/ForumTopic'));
+const Login = React.lazy(() => import('../components/pages/login/Login'));
+const Register = React.lazy(() => import('../components/pages/register/Register'));
+const Profile = React.lazy(() => import('../components/pages/profile/Profile'));
+const MembersDirectory = React.lazy(() => import('../components/pages/membersDirectory/MembersDirectory'));
+const SearchPopUp = React.lazy(() => import('./search/SearchPopUp'));
+const CookiePolicy = React.lazy(() => import('../components/pages/legal/CookiePolicy'));
+const PrivacyPolicy = React.lazy(() => import('../components/pages/legal/PrivacyPolicy'));
+const TermsAndConditions = React.lazy(() => import('../components/pages/legal/TermsAndConditions'));
+const UnauthorisedPermissions = React.lazy(() => import('../components/pages/unauthorised/UnauthorisedPermissions'));
+const CookieNotification = React.lazy(() => import('../components/pages/legal/CookieNotification'));
+const Footer = React.lazy(() => import('../components/footer/Footer'));
+const Error404 = React.lazy(() => import('../components/pages/errors/Error404'));
+
 
 const App = () => {
   return (
     <>
       <ConstProvider>
+      <Suspense fallback={<div className="loaderSpinner"><img alt="Loading Spinner" src={spinner}/></div>}>
         <Header />
         <SideNavMobile />
         <div className="container">
@@ -66,6 +68,7 @@ const App = () => {
         <CookieNotification />
         <SearchPopUp />
         <Footer />
+        </Suspense>
         </ConstProvider>
     </>
   );
